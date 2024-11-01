31865c344b5826ffda5f16c6da2d9b8d6547a56f
Josema <jm3develop@gmail.com>
2024-11-01 18:54:17 +0100

    added book and review model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,44 @@ class Book extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg(
+            ['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)],
+            'rating'
+        )
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    //need to call the popular method before in order to get the review_count row and access it
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->where('reviews_count', '>=', $minReviews);
+    }
+
+    //no need to return anything because $query is an object, and objects are passsed by reference to functions
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } else if (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } else if ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
 }
