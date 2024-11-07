73de0768054560b73e70e6b4c2d45e1b81d4658f
Josema <jm3develop@gmail.com>
2024-11-07 13:23:02 +0100

    finished

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::resource('books', BookController::class)->only(['index', 'show']);
 Route::get('books/{book}/reviews/create', [ReviewController::class, 'create'])
     ->name('books.reviews.create');
 
+
 Route::post('books/{book}/reviews', [ReviewController::class, 'store'])
     ->name('books.reviews.store')
     ->middleware('throttle:reviews');
